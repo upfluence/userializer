97f8d75feeca77e86b245c975ebf61a52d58461a
Alexis Montagne <alexis.montagne@gmail.com>
2019-05-10 11:56:10 -0400

    userializer: Handle nil object for serialization

--- lib/userializer.rb
+++ lib/userializer.rb
@@ -7,10 +7,14 @@ module USerializer
 
   class << self
     def serializer_for(obj)
+      return nil if kls.nil?
+
       infered_serializer_class(obj.class)
     end
 
     def infered_serializer_class(kls)
+      return nil if kls.nil?
+
       (kls.name + 'Serializer').split(NS_SEPARATOR).inject(Object) do |o, c|
         o.const_get(c)
       end
